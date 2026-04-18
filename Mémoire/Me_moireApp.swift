6bb9c96b1a9973fbef5b75e8f4fd05ef21a399d6
quentin-aslan <contact@quentinaslan.com>
2026-04-18 09:05:44 +0200

    Initial Commit

---
+++ Mémoire/Me_moireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Me_moireApp.swift
+//  Mémoire
+//
+//  Created by Quentin Aslan on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Me_moireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
